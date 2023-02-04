96ad6d12ced688855f5a1b0feac46618d46b1bc6
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2023-02-04 10:08:53 -0500

    first commit

---
+++ src/01-class.ts
@@ -0,0 +1,12 @@
+const date = new Date();
+date.getHours();
+date.getTime();
+date.toISOString();
+
+const date2 = new Date(1993,1,12); //0 = enero, 11 = diciembre
+date2.getHours();
+date2.getTime();
+date2.toISOString();
+
+console.log(date2);
+
