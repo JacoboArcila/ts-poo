a0821b063396146f94db15bfd00e63cda65642c0
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2023-02-04 10:17:30 -0500

    Class

--- src/01-class.ts
+++ src/01-class.ts
@@ -10,3 +10,23 @@ date2.toISOString();
 
 console.log(date2);
 
+let myVar;
+
+class MyDate {
+    year: number;
+    month: number;
+    day: number;
+
+    constructor(year: number, month: number, day: number) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+}
+
+const myDate = new MyDate(2023, 3, 14);
+
+console.log(myDate);
+
+
+
